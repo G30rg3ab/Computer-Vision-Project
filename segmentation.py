bbd2db69e7d1e5c3a025cc2cce341cfb9af8b7b0
G30rg3ab <143543161+G30rg3ab@users.noreply.github.com>
2025-03-01 17:03:15 +0000

    Unet

--- segmentation.py
+++ segmentation.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset as BaseDataset
 import matplotlib.pyplot as plt_
 import albumentations as albu
+from sklearn.model_selection import train_test_split # type: ignore
+from albumentations.pytorch import ToTensorV2
+import torch
+from torch.utils.data import DataLoader
 
 class CVDataset(BaseDataset):
     '''
     Dataset class for Semantic Segmentation
     '''
-    def __init__(self, images_dir, masks_dir, classes = None, augmentation = None, preprocessing = None):
+    def __init__(self, images_fps, masks_fps, augmentation = None, preprocessing = None):
         '''
         Initialize the Dataset.
 
@@ -32,18 +36,11 @@ class CVDataset(BaseDataset):
             A set of preprocessing transforms (e.g., normalization, resizing) 
             applied to both images and masks after augmentation. Applied only if provided.
         '''
-        class_intensity_dict = {'background':0, 'dog':75, 'cat':38}
-
-        # Get images (x) and masks (y). These are the names of how the images/labels are saved
-        self.ids_x = sorted(os.listdir(images_dir)) # Sorting image names to match with masks
-        self.ids_y = sorted(os.listdir(masks_dir)) # Sorting mask names to match with masks
-
-        # Get the full paths of images(X) and masks (y)
-        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids_x]
-        self.masks_fps  = [os.path.join(masks_dir, image_id) for image_id in self.ids_y ]
-
-        # Convert the str names to class values which will be on masks
-        self.class_values = [class_intensity_dict[cls] for cls in classes]
+        self.class_intensity_dict = {'background':0, 'dog':75, 'cat':38}
+        
+        # Setting full paths
+        self.images_fps = images_fps
+        self.masks_fps = masks_fps
 
         self.augmentation  = augmentation
         self.preprocessing = preprocessing
@@ -53,50 +50,191 @@ class CVDataset(BaseDataset):
         image = cv2.imread(self.images_fps[i])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(self.masks_fps[i], 0)
-        
-        # Extracting the classes from the labels (eg. cat)
-        masks = [(mask == v) for v in self.class_values]
-        mask = np.stack(masks, axis = -1).astype('float')
+
+        # Replacing class_intensity_dict values with their index
+        for idx, pixel_intensity in enumerate(self.class_intensity_dict.values()):
+            mask[mask == pixel_intensity] = idx
        
         if self.augmentation:
             sample = self.augmentation(image = image, mask = mask)
             image, mask = sample['image'], sample['mask']
         
+        # appy preprocessing
         if self.preprocessing:
-            raise NotImplementedError
+            sample = self.preprocessing(image = image, mask = mask)
+            image, mask = sample['image'], sample['mask']
         
         return image, mask
     
     def __len__(self):
-        return len(self.ids_x)
+        return len(self.images_fps)
 
-# Helper function for data visualisation
-def visualiseData(**image):
-    '''Plot images in one row'''
-    n = len(image)
-    plt_.figure(figsize = (10, 10))
 
-    for i, (name, image) in enumerate(image.items()):
-        plt_.subplot(1, n, i + 1)
-        plt_.xticks([])
-        plt_.yticks([])
-        plt_.title(' '.join(name.split('_')).title())
-        plt_.imshow(image)
-    plt_.show()
+class preprocessing():
 
-def get_training_augmentation():
-    train_transform = [
- 
+    @ staticmethod
+    def train_val_split(x_trainVal_dir, y_trainVal_dir, val_size = 0.2, random_state = 42):
+        # Getting the names of the images
+        images_idx = sorted(os.listdir(x_trainVal_dir)) # Sorting to match with labels
+        labels_idx = sorted(os.listdir(y_trainVal_dir)) # Sorting to match with images
+
+
+        # Splitting into training and validation
+        X_train, X_val, y_train, y_val = train_test_split(images_idx,
+                                                        labels_idx,
+                                                        test_size=val_size,
+                                                        random_state=random_state
+                                                        )
+        # Concatenating the data directory onto each of the names
+        X_train_fps = [os.path.join(x_trainVal_dir, image_idx) for image_idx in X_train]
+        y_train_fps = [os.path.join(y_trainVal_dir, label_idx) for label_idx in y_train]
+
+        X_val_fps = [os.path.join(x_trainVal_dir, image_idx) for image_idx in X_val]
+        y_val_fps = [os.path.join(y_trainVal_dir, label_idx) for label_idx in y_val]
+        
+        return X_train_fps, X_val_fps, y_train_fps, y_val_fps
+
+    @ staticmethod
+    def get_testing_paths(x_test_dir, y_test_dir):
+        # Getting the names of the images
+        images_idx = sorted(os.listdir(x_test_dir)) # Sorting to match with labels
+        labels_idx = sorted(os.listdir(y_test_dir)) # Sorting to match with images
+
+        x_test_fps = [os.path.join(x_test_dir, image_idx) for image_idx in images_idx]
+        y_test_fps = [os.path.join(y_test_dir, label_idx) for label_idx in labels_idx]
+
+        return x_test_fps, y_test_fps
+
+    @ staticmethod
+    def get_training_augmentation():
+        '''
+        This function returns an albumentations.Compose element 
+        which peforms augmentation during training.
+        '''
+        train_transform = [
+            albu.HorizontalFlip(p=0.5),
+    
+            albu.OneOf([
+                albu.RandomBrightnessContrast(brightness_limit=0.4, contrast_limit=0.4, p=1),
+                albu.CLAHE(p=1),
+                albu.HueSaturationValue(p=1)
+                ],
+                p=0.9,
+            ),
+            albu.GaussNoise(p=0.2),
+        ]
+        return albu.Compose(train_transform)
+    
+    @ staticmethod
+    def get_transforms():
+        transforms = albu.Compose([
         albu.Resize(256, 416, p=1),
-        albu.HorizontalFlip(p=0.5),
- 
-        albu.OneOf([
-            albu.RandomBrightnessContrast(brightness_limit=0.4, contrast_limit=0.4, p=1),
-            albu.CLAHE(p=1),
-            albu.HueSaturationValue(p=1)
-            ],
-            p=0.9,
-        ),
-        albu.GaussNoise(p=0.2),
-    ]
-    return albu.Compose(train_transform)
+        albu.Normalize(
+            mean = [0, 0, 0],
+            std = [1, 1, 1],
+            max_pixel_value = 255
+        ), ToTensorV2()
+        ])
+        return transforms
+        
+
+    
+class show():
+    @ staticmethod
+    def colorise_mask(mask, palette):
+        return palette[mask]
+
+    @ staticmethod
+    # Helper function for data visualisation
+    def visualiseData(**image):
+        '''Plot images in one row'''
+
+        # Create a 256×3 array initialized to (0,0,0) for "unused" entries
+        palette = np.zeros((256, 3), dtype=np.uint8)
+        # Assign colors for your known labels
+        palette[0]   = [0,   0,   0]    # class 0 => black
+        palette[1]   = [255, 0,   0]    # class 1 => red
+        palette[2]   = [0,   255, 0]    # class 2 => green
+        palette[255] = [255, 255, 255]  # ignore index => white
+      
+        n = len(image)
+        plt_.figure(figsize = (10, 10))
+
+        for i, (name, image) in enumerate(image.items()):
+            plt_.subplot(1, n, i + 1)
+            plt_.xticks([])
+            plt_.yticks([])
+            plt_.title(' '.join(name.split('_')).title())
+
+            if len(image.shape) == 2:
+                image = show.colorise_mask(image, palette)
+                # 3-channel color image
+            plt_.imshow(image)
+        plt_.show()
+
+class utils():
+    @staticmethod
+    def save_checkpoint(state, filename = 'my_checkpoint.pth.tar'):
+        print('=> Saving checkpoint')
+        torch.save(state, filename)
+    
+    @staticmethod
+    def load_checkpoint(checkpoint, model):
+        print('=> Loading checkpoint')
+        model.load_state_dict(checkpoint['state_dict'])
+
+    @staticmethod
+    def get_loaders(
+        trainVal_dir, 
+        trainVal_maskdir,
+        batch_size,
+        transform,
+        train_augmentation,
+        num_workers = 4, 
+        pin_memory = True
+    ):
+
+        # Splitting relative path names into into training and validation
+        x_train_fps, x_val_fps, y_train_fps, y_val_fps = preprocessing.train_val_split(trainVal_dir, trainVal_maskdir, 0.2)
+        # Initialising the data loaders
+        train_ds = CVDataset(x_train_fps, y_train_fps, augmentation = train_augmentation, preprocessing = transform)
+        train_loader = DataLoader(
+            train_ds,
+            batch_size= batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            shuffle=True
+        )
+
+        valid_ds = CVDataset(x_val_fps, y_val_fps, preprocessing = transform)
+        valid_loader = DataLoader(
+            valid_ds,
+            batch_size= batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            shuffle=False
+        )
+
+        return train_loader, valid_loader
+    
+    def check_accuracy(loader, model, device="cuda"):
+        num_correct = 0
+        num_pixels = 0
+        model.eval()
+
+        with torch.no_grad():
+            for x, y in loader:
+                x = x.to(device)
+                y = y.to(device)  # shape: (N, H, W), with integer labels in [0..(num_classes-1)]
+
+                # Forward pass: model outputs logits of shape (N, num_classes, H, W)
+                preds = model(x)  # shape: (N, C, H, W)
+
+                # Convert logits to predicted class indices
+                preds = torch.argmax(preds, dim=1)  # shape: (N, H, W)
+
+                # Compare preds with ground truth
+                num_correct += (preds == y).sum().item()
+                num_pixels  += torch.numel(preds)  # total number of pixels
+
+        print(f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels}")
---
+++ train.py
@@ -0,0 +1,91 @@
+import albumentations as albu
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+from model import UNET
+import os
+
+# # Pytorch imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch import amp
+
+# Custom
+from segmentation import utils
+from segmentation import preprocessing
+
+# Hyperparameters
+LEARNING_RATE = 1e-4
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+BATCHSIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+PIN_MEMORY = True
+LOAD_MODEL = False
+DATA_DIR = 'Dataset/'
+x_test_dir = os.path.join(DATA_DIR, 'Test/color')
+y_test_dir = os.path.join(DATA_DIR, 'Test/label')
+x_trainVal_dir = os.path.join(DATA_DIR, 'TrainVal/color')
+y_trainVal_dir = os.path.join(DATA_DIR, 'TrainVal/label')
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    '''
+    One epoch of training
+    '''
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device = DEVICE)
+        targets = targets.long().unsqueeze(1).to(device = DEVICE)
+
+        # forward
+        with torch.autocast(device_type='cuda', dtype=torch.float16):
+            targets = targets.to(device = DEVICE).squeeze(1)  # Now masks is (N,H,W)
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm loop
+        loop.set_postfix(loss = loss.item())
+
+def main():
+    model = UNET(in_channels=3, out_channels=3).to(DEVICE)
+    # Defining the loss function we use in training
+    loss_fn = nn.CrossEntropyLoss(ignore_index=255)
+    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+    # Getting the transforms 
+    transform = preprocessing.get_transforms()
+    train_augmentation = preprocessing.get_training_augmentation()
+
+    # Getting the loaders
+    train_loader, val_loader = utils.get_loaders(x_trainVal_dir,
+                                                 y_trainVal_dir,
+                                                 batch_size= BATCHSIZE,
+                                                 transform= transform,
+                                                 train_augmentation=train_augmentation, 
+                                                 num_workers=NUM_WORKERS,
+                                                 pin_memory=PIN_MEMORY)
+
+    scaler = amp.GradScaler()
+    for epoc in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        # save model
+        checkpoint = {
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+        utils.save_checkpoint(checkpoint)
+
+        # check accuracy
+        checkpoint(val_loader, model, device = DEVICE)
+
+
+if __name__ == '__main__':
+    main()
