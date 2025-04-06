e084acacdafde68382f99546a55c6c2d4f3e430f
G30rg3ab <143543161+G30rg3ab@users.noreply.github.com>
2025-04-06 16:58:02 +0100

    hyperparameter tuning

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Computer Vision Project
  Mini Project for University of Edinburgh Computer Vision course
+
+
+# Unet models
+2. No affine (learning rate starts at 1e-4 -> 1e-5), loss = CrossEntropy
+3. No affince (learning rate starts at 1e-4), loss = Dice
+4. Affine, Dice (0.5) & cross entropy (0.5), learning rate scheduler
--- segmentation/loss.py
+++ segmentation/loss.py
@@ -29,3 +29,20 @@ class DiceLoss(nn.Module):
             loss += 1 - dice_score  # 1 - Dice = loss
 
         return loss / len(self.classes)
+    
+
+class DiceCELoss(nn.Module):
+    """Combine Cross-Entropy and Dice for segmentation."""
+    def __init__(self, dice_weight=1.0, ce_weight=1.0, classes = [0, 1, 2]):
+        super().__init__()
+        self.dice_weight = dice_weight
+        self.ce_weight = ce_weight
+        self.dice_loss = DiceLoss(classes=classes)
+        self.ce_loss = nn.CrossEntropyLoss(ignore_index=255) 
+
+    def forward(self, logits, targets):
+        # logits shape (N, C, H, W) and targets shape (N, H, W) if multiclass
+        ce = self.ce_loss(logits, targets)
+        dice = self.dice_loss(logits, targets)
+
+        return self.dice_weight * dice + self.ce_weight * ce
